e49c20e9de9499ad9521276e7e01d11295e3eb33
NezukoTierna <matiasorellanahormazabal@gmail.com>
2024-03-22 08:53:58 -0300

    Added User.cs

---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+namespace Ayudantia1.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public String name { get; set; }
+
+        public String email { get; set; }
+
+        public String password  { get; set; }
+    }
+}
