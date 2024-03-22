cc55591c4a2c521c0b35fee29a268f4c1ec96314
NezukoTierna <matiasorellanahormazabal@gmail.com>
2024-03-22 09:15:26 -0300

    Program.cs Corrected

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using Ayudantia1.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-builder.Services.AddDbContext<
+builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlite("DataSource = Ayudantia1"));
 
 app.MapGet("/", () => "Hello World!");
 
