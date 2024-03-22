bbe1340330e096ac027eb3f3a60d9e08014e55f2
NezukoTierna <matiasorellanahormazabal@gmail.com>
2024-03-22 09:10:40 -0300

    added DataContext.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+builder.Services.AddDbContext<
+
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
---
+++ Src/Data/DataContext.cs
@@ -0,0 +1,19 @@
+using Ayudantia1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ayudantia1.Context
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<User> users { get; set;} = null;
+
+        public DbSet<Role> roles { get; set;} = null;
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) {}
+        {
+            
+        }
+
+    }
+}
